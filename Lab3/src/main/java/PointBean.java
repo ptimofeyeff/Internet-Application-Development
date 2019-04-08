1711a6b3e78a361763c861b36f1c72f4a34840bf
Pavel Timofeev <timos500@mail.ru>
2019-04-08 21:37:19 +0300

    add handle of results

--- Lab3/src/main/java/PointBean.java
+++ Lab3/src/main/java/PointBean.java
@@ -1,12 +1,27 @@
 import lombok.Data;
+import model.Result;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import java.util.ArrayList;
+import java.util.List;
 
+@ViewScoped
 @Data
 @ManagedBean
 public class PointBean {
-    private double x;
-    private double y;
-    private double r;
+
+    private Result newPoint = new Result();
+    private List<Result> points = new ArrayList<Result>();
+
+
+    public void addPoint() {
+        newPoint.setResult();
+        points.add(newPoint);
+        newPoint = new Result();
+
+    }
+
+
 
 }
---
+++ Lab3/src/main/java/model/Result.java
@@ -0,0 +1,18 @@
+package model;
+
+import lombok.Data;
+
+@Data
+public class Result {
+    private double x;
+    private double y;
+    private double r;
+    private boolean result;
+
+    public void setResult() {
+        result = ((x <= 0 && y >= 0 && x >= r && y <= r)
+                || (x >= 0 && y <= 0 && 2*y >= x - r) ||
+                ((Math.pow(x, 2) + Math.pow(y, 2) <= r) && y <= 0 && x <= 0));
+    }
+
+}
