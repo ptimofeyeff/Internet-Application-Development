9d5b033ed92cb9be874d54c05eacadfca437abcf
Pavel Timofeev <timos500@mail.ru>
2019-04-15 03:39:59 +0300

    fix bag in setResult()

--- Lab3/src/main/java/model/Result.java
+++ Lab3/src/main/java/model/Result.java
@@ -10,7 +10,7 @@ public class Result {
     private boolean result;
 
     public void setResult() {
-        result = ((x <= 0 && y >= 0 && x >= r && y <= r)
+        result = ((x <= 0 && y >= 0 && x >= -r/2 && y <= r)
                 || (x >= 0 && y <= 0 && 2*y >= x - r) ||
                 ((Math.pow(x, 2) + Math.pow(y, 2) <= r) && y <= 0 && x <= 0));
     }
