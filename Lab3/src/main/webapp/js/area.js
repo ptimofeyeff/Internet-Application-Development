256f4111cfe3edcab6a5b0700fbe14061765cac4
Pavel Timofeev <timos500@mail.ru>
2019-04-21 17:19:50 +0300

    add dynamic points to area

--- Lab3/src/main/webapp/js/area.js
+++ Lab3/src/main/webapp/js/area.js
@@ -1,20 +1,45 @@
-const area = document.querySelector("#area");
+const graph = document.querySelector("#graph");
 
-function paintPoint(event){
-
+function addPoint(event){
     let radius = document.querySelector('#R').value;
 
     let svgX = event.offsetX;
     let svgY = event.offsetY;
-    let graphX =  Math.round((svgX - (area.width.baseVal.value/2))*0.005*radius*1000)/1000;
-    let graphY = Math.round((-(svgY - (area.height.baseVal.value/2)))*0.005*radius*1000)/1000;
+    let graphX =  Math.round((svgX - (graph.width.baseVal.value/2))*0.005*radius*1000)/1000;
+    let graphY = Math.round((-(svgY - (graph.height.baseVal.value/2)))*0.005*radius*1000)/1000;
 
     document.querySelector("#pointX").value = graphX;
     document.querySelector("#pointY").value = graphY;
     document.querySelector("#pointR").value = radius;
-
     document.querySelector("#submitPoint").click();
+
+
+    paintPoint(svgX, svgY);
 }
+
+
+function paintPoint(x, y){
+    const svgNS = "http://www.w3.org/2000/svg";
+    let point = document.createElementNS(svgNS,"circle");
+
+    point.setAttribute("r", "3");
+    point.setAttribute("cx", x);
+    point.setAttribute("cy", y);
+    point.setAttribute("fill", "red");
+    graph.appendChild(point);
+}
+
+function paintPointonCanvas(x, y){
+   let contextPoints = canvasPoints.getContext("2d");
+
+   contextPoints.beginPath();
+   contextPoints.fillStyle = 'red';
+   contextPoints.arc(x, y, 2,0,2*Math.PI, true);
+   contextPoints.stroke();
+   contextPoints.fill();
+
+}
+
 
 function repaintPoint(point) {
 
